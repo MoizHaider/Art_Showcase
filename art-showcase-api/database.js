d9cb60098e775036bbd6f8a702c11110fd7f503f
Moiz Haider <moizhaider70@gmail.com>
2024-04-08 12:18:12 +0500

    change mongodb file

--- art-showcase-api/database.js
+++ art-showcase-api/database.js
@@ -1,29 +1,39 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
-require('dotenv').config({ path: '.env.local' })
-
+require("dotenv").config({ path: ".env.local" });
 
 let db;
 
 exports.mongoConnect = (cb) => {
-    const dbUrl = process.env.MONGODB_URI;
-  MongoClient.connect(
-    `${dbUrl}`,
-    {
-      ssl: true,
-      serverSelectionTimeoutMS: 10000,
-    }).then(client=>{
-        db = client.db("ArtGallery")
-        cb()
-    }).catch(err=>{
-        console.log(err)
+  const dbUrl = process.env.MONGODB_URI;
+  MongoClient.connect(`${dbUrl}`, {
+    ssl: true,
+    serverSelectionTimeoutMS: 10000,
+  })
+    .then((client) => {
+      db = client.db("ArtGallery");
+      cb();
+    })
+    .catch((err) => {
+      console.log(err);
     });
 };
 
-exports.dbConnect = ()=>{
-    if(db){
-        return db;
-    }
-    throw "Database not foking found";
-}
-
+exports.dbConnect = () => {
+  if (db) {
+    return db;
+  } else {
+    const dbUrl = process.env.MONGODB_URI;
+    MongoClient.connect(`${dbUrl}`, {
+      ssl: true,
+      serverSelectionTimeoutMS: 10000,
+    })
+      .then((client) => {
+        db = client.db("ArtGallery");
+        cb();
+      })
+      .catch((err) => {
+        throw "Database not foking found";
+      });
+  }
+};
