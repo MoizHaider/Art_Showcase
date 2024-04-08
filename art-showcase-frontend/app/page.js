296efaa849b5f6da906a3eea8d072537ae4ec055
Moiz Haider <moizhaider70@gmail.com>
2024-04-08 11:07:52 +0500

    del file

--- art-showcase-frontend/app/page.js
+++ art-showcase-frontend/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Events from "./components/home/Events";
+
 
 import LoadMorePosts from "./components/LoadMorePosts";
 import SearchSection from "./components/SearchSection";
